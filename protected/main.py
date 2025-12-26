ab8a3c10065e7139ce8f690e45820085b9d64c7b
mopsipriv <jarvinenandrei@gmail.com>
2025-12-26 12:51:18 +0200

    Added logging system

--- protected/main.py
+++ protected/main.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(
+    filename="info.log",
+    format="%(asctime)s %(message)s",
+    level=logging.WARNING
+)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     conn = get_db_connection()
+    ip = request.remote_addr
 
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+        username = request.form.get("username", "")
+        password = request.form.get("password", "")
+
+        username_l = username.lower()
+        password_l = password.lower()
+
+        suspicious_patterns = ["--", "union", "' or"]
+
+        for pattern in suspicious_patterns:
+            if pattern in username_l or pattern in password_l:
+                logging.warning(
+                    f"Possible SQLi attempt from {ip}: {username} | {password}")
+                return render_template("login.html",error="Suspicious input detected")
 
         query = "SELECT * FROM initials WHERE username = ? AND password = ?"
         result = conn.execute(query, (username, password)).fetchone()
@@ -17,7 +36,7 @@ def login():
         if result:
             return redirect(url_for("welcome"))
         else:
-            return render_template("login.html", error="Invalid username or password")
+            return render_template("login.html",error="Invalid username or password" )
 
     return render_template("login.html")
 
